1873fcab28cdb2c316a7093483c70234371930e1
Derk Zomer <derk@shotstack.io>
2021-09-30 18:51:57 +1000

    Remove views

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
-app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {});
